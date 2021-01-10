a1f2456d5181870cda3d1d36dce44fc1c12bd7ee
Anaëlle <anaellepierredon@gmail.com>
2021-01-10 17:40:21 +0100

    ajout commentaires

--- Java-Devoir-S1/src/traitementTextes/bibliotheque/Amende.java
+++ Java-Devoir-S1/src/traitementTextes/bibliotheque/Amende.java
@@ -1,16 +1,32 @@
 package traitementTextes.bibliotheque;
 
+/**
+* 
+* @author Anna Niskovskikh, Anaëlle Pierredon et Qi Wang
+* @version 1.3
+*/
+
 public class Amende {
 	
 	private int nbJourEnRetard;
 	private double amende;
 	private static final int prixParJour = 1 ;
 
+	
+	/**
+    * Constructeur de la classe Amende
+    * @param nbJourEnRetard Le nombre de jours de retard dans le rendu du livre.
+    */
 	public Amende(int nbJourEnRetard) {
 		this.nbJourEnRetard = nbJourEnRetard;
 		this.amende = calculer(nbJourEnRetard);
 	}
 	
+	/**
+    * Méthode pour calculer le montant de l'amende en fonction du nombre de jours en retard.
+    * Le prix par jour est fixé à 1 euro.
+    * @param nbJourEnRetard Le nombre de jours de retard dans le rendu du livre.
+    */
 	public int calculer(int nbJourEnRetard) {
 		return prixParJour * nbJourEnRetard;
 		}
--- Java-Devoir-S1/src/traitementTextes/bibliotheque/Auteur.java
+++ Java-Devoir-S1/src/traitementTextes/bibliotheque/Auteur.java
@@ -1,19 +1,31 @@
 package traitementTextes.bibliotheque;
 
+/**
+* 
+* @author Sandrine Cariteau
+*/
+
 import java.io.Serializable;
 
 public class Auteur implements Serializable {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	private String nom;
-
+	
+	/**
+	 * Constructeur de la classe Auteur
+	 * @param nom
+	 */
 	public Auteur(String nom) {
 		this.setNom(nom);
 	}
-
+	
+	/**
+	 * Méthode equals pour les objets de la classe Auteur. 
+	 * Elle vérifie que les noms de deux objets de la classe Auteur sont égaux.
+	 * @param anObject
+	 */
 	@Override
 	public boolean equals(Object anObject) {
 		if (this == anObject) {
@@ -25,7 +37,10 @@ public class Auteur implements Serializable {
 		return false;
 
 	}
-
+	
+	/**
+	 * 
+	 */
 	public int hashCode() {
 		return nom.hashCode();
 	}
